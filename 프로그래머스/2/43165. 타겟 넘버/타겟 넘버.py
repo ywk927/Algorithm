bed7cf99b1238a7d1ad06d01eabf84fe96a53cf5
YoungWoo Ko <dreamkywww@gmail.com>
2026-05-13 17:12:09 +0900

    [level 2] Title: 타겟 넘버, Time: 178.15 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    def dfs(index, total):
+        nonlocal answer
+        if index == len(numbers):
+            if total == target:
+                answer += 1
+            return
+        dfs(index + 1, total + numbers[index])
+        dfs(index + 1, total - numbers[index])
+    dfs(0,0)
+    return answer
