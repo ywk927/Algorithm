94a8c34a585b765f762027d68d70ed4076cd85d5
YoungWoo Ko <dreamkywww@gmail.com>
2026-06-13 15:08:41 +0900

    [level 2] Title: 할인 행사, Time: 67.63 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,19 +1,34 @@
+from collections import Counter
 def solution(want, number, discount):
     answer = 0
-    m = len(want)
-    real_want = []
-    for i in range(m):
-        for _ in range(number[i]):
-            real_want.append(want[i])
-    real_want.sort()
-    n = len(discount)
-    index = 0
-    while index <= n-10:
-        expected_discount = []
-        for i in range(index, index+10):
-            expected_discount.append(discount[i])
-        expected_discount.sort()
-        if real_want == expected_discount:
+    # m = len(want)
+    # real_want = []
+    # for i in range(m):
+    #     for _ in range(number[i]):
+    #         real_want.append(want[i])
+    # real_want.sort()
+    # n = len(discount)
+    # index = 0
+    # while index <= n-10:
+    #     expected_discount = []
+    #     for i in range(index, index+10):
+    #         expected_discount.append(discount[i])
+    #     expected_discount.sort()
+    #     if real_want == expected_discount:
+    #         answer += 1
+    #     index += 1
+    n = len(want)
+    dict = {}
+    for i in range(n):
+        dict[want[i]] = number[i]
+    m = len(discount)
+    for i in range(m-9):
+        arr ={}
+        for j in range(i, i+10):
+            if discount[j] in arr:
+                arr[discount[j]] += 1
+            else:
+                arr[discount[j]] = 1
+        if arr == dict :
             answer += 1
-        index += 1
     return answer
